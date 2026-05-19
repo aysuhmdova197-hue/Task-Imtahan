2c635926945e955395125148e3a7cb369c2b57b2
aysufa-azmpa202-create <aysufa-azmpa202@code.edu.az>
2026-05-19 19:11:00 +0400

    imtahan task

---
+++ FinalExam/FinalExam/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FinalExam.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalExam.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
