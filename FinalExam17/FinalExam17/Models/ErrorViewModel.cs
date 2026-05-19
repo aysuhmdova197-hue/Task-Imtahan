f68ba4391ed88793acd3022b7fd394839d0b93bd
aysufa-azmpa202-create <aysufa-azmpa202@code.edu.az>
2026-05-19 19:18:33 +0400

    FinalExam17 qovlugu tamamile silindi

--- FinalExam17/FinalExam17/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalExam17.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
